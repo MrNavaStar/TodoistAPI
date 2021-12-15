0fd94a50bb3cd8a73f3ec496ddedb8c974b304c6
Ethan Stevenson <Mr.NavaStar@gmail.com>
2021-12-14 20:27:30 -1000

    Remove need for restlet

--- src/main/java/mrnavastar/todoistapi/TodoistAPI.java
+++ src/main/java/mrnavastar/todoistapi/TodoistAPI.java
@@ -1,8 +1,17 @@
 package mrnavastar.todoistapi;
 
+import mrnavastar.todoistapi.api.Project;
+import mrnavastar.todoistapi.api.Todoist;
+
 public class TodoistAPI {
 
     public static void main(String[] args) {
+        Todoist todoist = new Todoist("869ccfdcb50f0c582237a6f32760936466574db0");
+        todoist.sync();
 
+        Project project = todoist.getProject("Admin");
+        project.createTask("New Task").setSection("Issues").setPriority(4);
+
+        todoist.commit();
     }
 }
--- src/main/java/mrnavastar/todoistapi/api/Todoist.java
+++ src/main/java/mrnavastar/todoistapi/api/Todoist.java
@@ -1,43 +1,61 @@
 package mrnavastar.todoistapi.api;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.restlet.Client;
-import org.restlet.data.*;
-import org.restlet.resource.ClientResource;
 
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class Todoist {
 
-    private final Client client;
-    private final ChallengeResponse authHeader;
+    private final HttpClient client;
+    private final String token;
     private final HashMap<String, Project> projects = new HashMap<>();
     private final HashMap<Long, Section> sections = new HashMap<>();
     private final HashMap<String, Label> labels = new HashMap<>();
     private final HashMap<Long, Task> tasks = new HashMap<>();
 
     public Todoist(String token) {
-        this.client = new Client(Protocol.HTTP);
-        this.authHeader = new ChallengeResponse(ChallengeScheme.HTTP_OAUTH_BEARER);
-        authHeader.setRawValue(token);
-        authHeader.setIdentifier("Bearer");
+        this.client = HttpClient.newHttpClient();
+        this.token = token;
     }
 
     private JsonElement get(String url) {
-        ClientResource clientResource = new ClientResource(url);
-        clientResource.setChallengeResponse(this.authHeader);
-        clientResource.setMethod(Method.GET);
-
-        return JsonParser.parseString(client.handle(clientResource.createRequest()).getEntityAsText());
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(url))
+                    .setHeader("Authorization", "Bearer " + token)
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return JsonParser.parseString(response.body());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void post(String url, HashMap<String, Object> properties) {
-        ClientResource clientResource = new ClientResource(url);
-        clientResource.setChallengeResponse(this.authHeader);
-        clientResource.post(properties);
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(new Gson().toJson(properties)))
+                    .uri(URI.create(url))
+                    .setHeader("Content-Type", "application/json")
+                    .setHeader("X-Request-Id", UUID.randomUUID().toString())
+                    .header("Authorization", "Bearer " + token)
+                    .build();
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sync() {
@@ -50,36 +68,43 @@ public class Todoist {
         JsonElement labelData = get("https://api.todoist.com/rest/v1/labels");
         JsonElement taskData = get("https://api.todoist.com/rest/v1/tasks");
 
-
-        for (JsonElement p : projectData.getAsJsonArray()) {
-            JsonObject data = p.getAsJsonObject();
-            String name = data.get("name").getAsString();
-            this.projects.put(name, new Project(this, name, data.get("id").getAsLong()));
+        if (projectData != null) {
+            for (JsonElement p : projectData.getAsJsonArray()) {
+                JsonObject data = p.getAsJsonObject();
+                String name = data.get("name").getAsString();
+                this.projects.put(name, new Project(this, name, data.get("id").getAsLong()));
+            }
         }
 
-        for (JsonElement s : sectionData.getAsJsonArray()) {
-            JsonObject data = s.getAsJsonObject();
-            long id = data.get("id").getAsLong();
-            this.sections.put(id, new Section(data.get("name").getAsString(), id, data.get("project_id").getAsLong()));
+        if (sectionData != null) {
+            for (JsonElement s : sectionData.getAsJsonArray()) {
+                JsonObject data = s.getAsJsonObject();
+                long id = data.get("id").getAsLong();
+                this.sections.put(id, new Section(data.get("name").getAsString(), id, data.get("project_id").getAsLong()));
+            }
         }
 
-        for (JsonElement l : labelData.getAsJsonArray()) {
-            JsonObject data = l.getAsJsonObject();
-            String name = data.get("name").getAsString();
-            this.labels.put(name, new Label(name, data.get("id").getAsLong()));
+        if (labelData != null) {
+            for (JsonElement l : labelData.getAsJsonArray()) {
+                JsonObject data = l.getAsJsonObject();
+                String name = data.get("name").getAsString();
+                this.labels.put(name, new Label(name, data.get("id").getAsLong()));
+            }
         }
 
-        for (JsonElement t : taskData.getAsJsonArray()) {
-            JsonObject data = t.getAsJsonObject();
+        if (taskData != null) {
+            for (JsonElement t : taskData.getAsJsonArray()) {
+                JsonObject data = t.getAsJsonObject();
 
-            HashMap<String, Object> properties = new HashMap<>();
-            properties.put("id", data.get("id").getAsLong());
-            properties.put("project_id", data.get("project_id").getAsLong());
-            properties.put("content", data.get("content").getAsString());
-            properties.put("description", data.get("description").getAsString());
-            properties.put("priority", data.get("priority").getAsInt());
+                HashMap<String, Object> properties = new HashMap<>();
+                properties.put("id", data.get("id").getAsLong());
+                properties.put("project_id", data.get("project_id").getAsLong());
+                properties.put("content", data.get("content").getAsString());
+                properties.put("description", data.get("description").getAsString());
+                properties.put("priority", data.get("priority").getAsInt());
 
-            this.tasks.put((Long) properties.get("id"), new Task(this, properties));
+                this.tasks.put((Long) properties.get("id"), new Task(this, properties));
+            }
         }
     }
 
