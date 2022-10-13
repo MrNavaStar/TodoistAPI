c4fbd041d9be87baf61b19368ba3695da1d2af25
Ethan Stevenson <43613843+MrNavaStar@users.noreply.github.com>
2022-10-13 13:09:56 -0600

    Update TodoistAPI.java

--- src/main/java/mrnavastar/todoistapi/TodoistAPI.java
+++ src/main/java/mrnavastar/todoistapi/TodoistAPI.java
@@ -6,7 +6,7 @@ import mrnavastar.todoistapi.api.Todoist;
 public class TodoistAPI {
 
     public static void main(String[] args) {
-        Todoist todoist = new Todoist("869ccfdcb50f0c582237a6f32760936466574db0");
+        Todoist todoist = new Todoist("");
         todoist.sync();
 
         Project project = todoist.getProject("Admin");
